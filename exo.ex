b64cc942eaaaf743c172667cb8b1b8ca2058c136
xieyuheng <xyheme@gmail.com>
2018-04-11 19:43:26 +0800

    goal

--- exo.ex
+++ exo.ex
@@ -1,9 +1,9 @@
 defmodule Exo do
   defmodule Var do
-    defstruct id: nil
+    defstruct id: 0
   end
 
-  def var id do
+  def var_c id do
     # -> Nat -- Var
     %Var{id: id}
   end
@@ -15,6 +15,8 @@ defmodule Exo do
       _ -> false
     end
   end
+
+  # Substitution = Var Term Map
 
   def walk u, s do
     # -> Term, Substitution -- Term
@@ -47,9 +49,69 @@ defmodule Exo do
     end
   end
 
-  # def disj x, y do
-  #   x + y
-  # end
+  defmodule State do
+    defstruct id_counter: 0, substitution: %{}
+  end
+
+  def state_c c, s do
+    # -> Nat, Substitution -- State
+    %State{id_counter: c, substitution: s}
+  end
+
+  def empty_state do
+    # -> -- State
+    %State{}
+  end
+
+  # Goal = (-> State -- State Stream)
+
+  def eqo u, v do
+    # -> Trem, Trem -- Goal
+    fn state ->
+      s = unify u, v, Map.get(state, :substitution)
+      if s do
+        [%State{state | substitution: s}]
+      else
+        []
+      end
+    end
+  end
+
+  def call_with_fresh fun do
+    # -> (-> Var -- Goal) -- Goal
+    fn state ->
+      id = Map.get(state, :id_counter)
+      goal = fun.(var_c(id))
+      goal.(%State{state | id_counter: id+1})
+    end
+  end
+
+  def disj g1, g2 do
+    # -> Goal, Goal -- Goal
+    fn state ->
+      s1 = g1.(state)
+      s2 = g2.(state)
+      mplus s1, s2
+    end
+  end
+
+  def conj g1, g2 do
+    # -> Goal, Goal -- Goal
+    fn state ->
+      s1 = g1.(state)
+      bind s1, g2
+    end
+  end
+
+  def mplus s1, s2 do
+    # -> State Stream, State Stream -- State Stream
+    Stream.concat s1, s2
+  end
+
+  def bind s, g do
+    # -> State Stream, Goal -- State Stream
+    s |> Stream.map(g) |> Stream.concat
+  end
 end
 
 ExUnit.start
@@ -57,39 +119,71 @@ ExUnit.start
 defmodule Exo.Text do
   use ExUnit.Case, async: true
 
+  import Exo
+
   test "var" do
-    assert Exo.var 0 |> Exo.var?
-    assert Exo.var 1 |> Exo.var?
-    assert Exo.var 2 |> Exo.var?
-    assert Exo.var 0 === Exo.var 0
+    assert var_c(0) |> var?
+    assert var_c(1) |> var?
+    assert var_c(2) |> var?
+    assert var_c(0) === var_c(0)
   end
 
   test "unify" do
-    s = Exo.unify [:a, :b, :c], [:a, :b, :c], %{}
+    s = unify [:a, :b, :c], [:a, :b, :c], %{}
     assert s === %{}
-    s = Exo.unify [[:a, :b, :c],
-                   [:a, :b, :c],
-                   [:a, :b, :c]],
+
+    s = unify [[:a, :b, :c],
+               [:a, :b, :c],
+               [:a, :b, :c]],
       [[:a, :b, :c],
        [:a, :b, :c],
        [:a, :b, :c]], %{}
     assert s === %{}
-    s = Exo.unify [Exo.var(0), :b, :c], [:a, :b, :c], %{}
-    assert s === %{%Exo.Var{id: 0} => :a}
-    s = Exo.unify [[Exo.var(0), :b, :c],
-                   [:a, Exo.var(1), :c],
-                   [:a, :b, Exo.var(2)]],
+
+    s = unify [var_c(0), :b, :c], [:a, :b, :c], %{}
+    assert s === %{var_c(0) => :a}
+
+    s = unify [[var_c(0), :b, :c],
+               [:a, var_c(1), :c],
+               [:a, :b, var_c(2)]],
       [[:a, :b, :c],
        [:a, :b, :c],
        [:a, :b, :c]], %{}
-    assert s === %{%Exo.Var{id: 0} => :a,
-                   %Exo.Var{id: 1} => :b,
-                   %Exo.Var{id: 2} => :c}
+    assert s === %{var_c(0) => :a,
+                   var_c(1) => :b,
+                   var_c(2) => :c}
   end
 
-  test "state" do
+  test "goal" do
+    goal = call_with_fresh fn a -> eqo a, 5 end
+    stream = empty_state() |> goal.()
+    assert stream === [state_c(1, %{var_c(0) => 5})]
+
+    goal = eqo [1, 2, 3], [1, 2, 3]
+    stream = empty_state() |> goal.()
+    assert stream === [state_c(0, %{})]
+
+    goal = eqo [1, 2, 3], [3, 2, 1]
+    stream = empty_state() |> goal.()
+    assert stream === []
   end
 
-  test "stream" do
+  def a_and_b do
+    g1 = call_with_fresh fn a -> eqo a, 7 end
+    g2 = call_with_fresh fn b ->
+      disj(eqo(b, 5), eqo(b, 6))
+    end
+    conj g1, g2
+  end
+
+  test "a and b" do
+    goal = a_and_b()
+    stream = empty_state() |> goal.()
+    list = Enum.take stream, 100
+    assert length(list) === 2
+    assert list === [
+      state_c(2, %{var_c(0) => 7, var_c(1) => 5}),
+      state_c(2, %{var_c(0) => 7, var_c(1) => 6}),
+    ]
   end
 end
