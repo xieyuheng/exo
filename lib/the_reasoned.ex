dda8d2f6c42de6123b5eb97661c226704164bc3a
xieyuheng <xyheme@gmail.com>
2018-05-12 23:02:00 +0800

    *** org-mode for the_reasoned.ex

--- lib/the_reasoned.ex
+++ lib/the_reasoned.ex
@@ -1,231 +1,225 @@
 defmodule TheReasoned do
-  @moduledoc """
-  The Reasoned Schemer in Elixir.
-  """
 
-  import Exo
+@moduledoc """
+The Reasoned Schemer in Elixir.
+"""
 
-  def cons(a, d) do
-    [a | d]
+import Exo
+
+def cons(a, d) do
+  [a | d]
+end
+
+def car(p) do
+  hd(p)
+end
+
+def cdr(p) do
+  tl(p)
+end
+
+def null?(x) do
+  x === []
+end
+
+def pair?(p) do
+  case p do
+    [_ | _] -> true
+    _ -> false
   end
+end
 
-  def car(p) do
-    hd(p)
+def list?(l) do
+  cond do
+    null?(l) -> true
+    pair?(l) -> list?(cdr(l))
+    :else -> false
   end
+end
 
-  def cdr(p) do
-    tl(p)
-  end
+def conso(a, d, p) do
+  cons(a, d) <~> p
+end
 
-  def conso(a, d, p) do
+def caro(p, a) do
+  fresh [d] do
     cons(a, d) <~> p
   end
+end
 
-  def caro(p, a) do
+def cdro(p, d) do
+  fresh [a] do
+    cons(a, d) <~> p
+  end
+end
+
+def nullo(x) do
+  x <~> []
+end
+
+def pairo(p) do
+  fresh [a, d] do
+    cons(a, d) <~> p
+  end
+end
+
+def listo(l) do
+  oro do
+    nullo(l)
     fresh [d] do
-      cons(a, d) <~> p
+      pairo(l)
+      cdro(l, d)
+      listo(d)
     end
   end
+end
 
-  def cdro(p, d) do
-    fresh [a] do
-      cons(a, d) <~> p
-    end
+def lol?(l) do
+  cond do
+    null?(l) -> true
+    list?(car(l)) -> lol?(cdr(l))
+    :else -> false
   end
+end
 
-  def null?(x) do
-    x === []
-  end
-
-  def nullo(x) do
-    x <~> []
-  end
-
-  def pair?(p) do
-    case p do
-      [_ | _] -> true
-      _ -> false
-    end
-  end
-
-  def pairo(p) do
+def lolo(l) do
+  oro do
+    nullo(l)
     fresh [a, d] do
-      cons(a, d) <~> p
+      caro(l, a)
+      listo(a)
+      cdro(l, d)
+      lolo(d)
     end
   end
+end
 
-  def list?(l) do
-    cond do
-      null?(l) -> true
-      pair?(l) -> list?(cdr(l))
-      :else -> false
+def twinso(s) do
+  fresh [x] do
+    [x, x] <~> s
+  end
+end
+
+def loto(l) do
+  oro do
+    nullo(l)
+    fresh [a, d] do
+      caro(l, a)
+      twinso(a)
+      cdro(l, d)
+      loto(d)
     end
   end
+end
 
-  #               The First Commandment
-  #   To transform a function whose value is a boolean
-  # into a function whose value is a goal, replace cond
-  # with conde and unnest each question and answer.
-
-  def listo(l) do
-    oro do
-      nullo(l)
-      fresh [d] do
-        pairo(l)
-        cdro(l, d)
-        listo(d)
-      end
+def listofo(predo, l) do
+  oro do
+    nullo(l)
+    fresh [a, d] do
+      caro(l, a)
+      predo.(a)
+      cdro(l, d)
+      listofo(predo, d)
     end
   end
+end
 
-  def lol?(l) do
-    cond do
-      null?(l) -> true
-      list?(car(l)) -> lol?(cdr(l))
-      :else -> false
+def member?(x, l) do
+  cond do
+    null?(l) -> false
+    eq_car?(l, x) -> true
+    :else -> member?(x, cdr(l))
+  end
+end
+
+def eq_car?(l, x) do
+  car(l) === x
+end
+
+def membero(x, l) do
+  oro do
+    caro(l, x)
+    fresh [d] do
+      cdro(l, d)
+      membero(x, d)
     end
   end
+end
 
-  def lolo(l) do
-    oro do
-      nullo(l)
-      fresh [a, d] do
-        caro(l, a)
-        listo(a)
-        cdro(l, d)
-        lolo(d)
-      end
-    end
-  end
-
-  def twinso(s) do
-    fresh [x] do
-      [x, x] <~> s
-    end
-  end
-
-  def loto(l) do
-    oro do
-      nullo(l)
-      fresh [a, d] do
-        caro(l, a)
-        twinso(a)
-        cdro(l, d)
-        loto(d)
-      end
-    end
-  end
-
-  def listofo(predo, l) do
-    oro do
-      nullo(l)
-      fresh [a, d] do
-        caro(l, a)
-        predo.(a)
-        cdro(l, d)
-        listofo(predo, d)
-      end
-    end
-  end
-
-  def member?(x, l) do
-    cond do
-      null?(l) -> false
-      eq_car?(l, x) -> true
-      :else -> member?(x, cdr(l))
-    end
-  end
-
-  def eq_car?(l, x) do
-    car(l) === x
-  end
-
-  def membero(x, l) do
-    oro do
+def pmembero(x, l) do
+  oro do
+    ando do
       caro(l, x)
-      fresh [d] do
-        cdro(l, d)
-        membero(x, d)
-      end
+      cdro(l, [])
+    end
+    fresh [d] do
+      cdro(l, d)
+      pmembero(x, d)
     end
   end
+end
 
-  def pmembero(x, l) do
-    oro do
-      ando do
-        caro(l, x)
-        cdro(l, [])
-      end
-      fresh [d] do
-        cdro(l, d)
-        pmembero(x, d)
-      end
+def first_value(l) do
+  run 1, y do
+    membero(y, l)
+  end
+end
+
+def memberrevo(x, l) do
+  oro do
+    fresh [d] do
+      cdro(l, d)
+      memberrevo(x, d)
+    end
+    caro(l, x)
+  end
+end
+
+def mem(x, l) do
+  cond do
+    null?(l) -> false
+    eq_car?(l, x) -> l
+    :else -> mem(x, cdr(l))
+  end
+end
+
+def memo(x, l, out) do
+  oro do
+    nullo(l)
+    ando do caro(l, x); l <~> out end
+    fresh [d] do
+      cdro(l, d)
+      memo(x, d, out)
     end
   end
+end
 
-  def first_value(l) do
-    run 1, y do
-      membero(y, l)
+def appendo(l, s, out) do
+  oro do
+    ando do nullo(l); out <~> s end
+    fresh [a, d, rec] do
+      conso(a, d, l)
+      conso(a, rec, out)
+      appendo(d, s, rec)
     end
   end
+end
 
-  def memberrevo(x, l) do
-    oro do
-      fresh [d] do
-        cdro(l, d)
-        memberrevo(x, d)
-      end
-      caro(l, x)
+def flatteno(s, out) do
+  oro do
+    ando do nullo(s)
+      [] <~> out
     end
-  end
-
-  def mem(x, l) do
-    cond do
-      null?(l) -> false
-      eq_car?(l, x) -> l
-      :else -> mem(x, cdr(l))
-    end
-  end
-
-  def memo(x, l, out) do
-    oro do
-      nullo(l)
-      ando do caro(l, x); l <~> out end
-      fresh [d] do
-        cdro(l, d)
-        memo(x, d, out)
+    ando do pairo(s)
+      fresh [a, d, res_a, res_d] do
+        conso(a, d, s)
+        flatteno(a, res_a)
+        flatteno(d, res_d)
+        appendo(res_a, res_d, out)
       end
     end
+    conso(s, [], out)
   end
-
-  # ><><><
-
-  def appendo(l, s, out) do
-    oro do
-      ando do nullo(l); out <~> s end
-      fresh [a, d, rec] do
-        conso(a, d, l)
-        conso(a, rec, out)
-        appendo(d, s, rec)
-      end
-    end
-  end
-
-  def flatteno(s, out) do
-    oro do
-      ando do nullo(s)
-        [] <~> out
-      end
-      ando do pairo(s)
-        fresh [a, d, res_a, res_d] do
-          conso(a, d, s)
-          flatteno(a, res_a)
-          flatteno(d, res_d)
-          appendo(res_a, res_d, out)
-        end
-      end
-      conso(s, [], out)
-    end
-  end
+end
 
 end
